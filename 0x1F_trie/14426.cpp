052e26e95142b1d26556b70e128956715c12fa92
hong <hsj0512@snu.ac.kr>
2024-12-26 22:43:40 +0900

    review 0x1F-trie

--- 0x1F_trie/14426.cpp
+++ 0x1F_trie/14426.cpp
@@ -1,25 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MX = 10000 * 501;
 const int ROOT = 1;
-const int MX = 10000 * 500 + 5;
 int unused = 2;
+int c2i(char c){ return c - 'a';}
 int nxt[MX][26];
-bool chk[MX];
-
-int c2i(char c){
-    return c - 'a';
-}
-
 void insert(string& s){
     int cur = ROOT;
     for (auto c : s){
-        if (nxt[cur][c2i(c)] == -1){
-            nxt[cur][c2i(c)] = unused++;
-        }
+        if (nxt[cur][c2i(c)] == -1) nxt[cur][c2i(c)] = unused++;
         cur = nxt[cur][c2i(c)];
     }
-    chk[cur] = true;
 }
 
 bool find(string& s){
@@ -34,20 +26,23 @@ bool find(string& s){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    for (int i = 0; i < MX; ++i) fill(nxt[i], nxt[i]+26, -1);
+
     int n,m;
     cin >> n >> m;
+    for (int i = 0; i < MX; ++i) fill(nxt[i], nxt[i]+26, -1);
     while (n--){
         string s;
         cin >> s;
         insert(s);
     }
+
     int ans = 0;
     while (m--){
         string s;
         cin >> s;
-        if (find(s)) ans++;
+        if (find(s)) ++ans;
     }
     cout << ans << '\n';
+
     return 0;
 }
--- 0x1F_trie/16934.cpp
+++ 0x1F_trie/16934.cpp
@@ -1,9 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MX = 100001 * 15;
+const int ROOT = 1;
+int nxt[MX][26];
+int cnt[MX];
+int c2i(char c){ return c - 'a'; }
+int unused = 2;
+
+string nickname(string& name){
+    int cur = ROOT;
+    string res = "";
+    bool chk = true;
+    for (int i = 0; i < name.size(); ++i){
+        char c = name[i];
+        if (chk) res += c;
+        if (nxt[cur][c2i(c)] == 0){
+            nxt[cur][c2i(c)] = unused++;
+            chk = false;
+        }
+        cur = nxt[cur][c2i(c)];
+    }
+    if (cnt[cur] == 0) cnt[cur] = 1;
+    else {
+        cnt[cur]++;
+        res += to_string(cnt[cur]);
+    }
+    return res;
+}
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    
+    int n;
+    cin >> n;
+    while (n--){
+        string name;
+        cin >> name;
+        cout << nickname(name) << '\n';
+    }
+    return 0;
 }
